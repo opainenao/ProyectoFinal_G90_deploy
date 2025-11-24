66ee4daf10e0d6e51fd2c7bdf4fb5e51bce9323f
Opainenao <opainenaomaldonado@gmail.com>
2025-11-24 19:14:03 -0300

    MODIF: reemplaza localhost por FRONTEND

--- Hito3-Back/server.js
+++ Hito3-Back/server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:5173',  
+  origin: FRONTEND_URL, //'http://localhost:5173',  
   credentials: true,
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type, Authorization"
